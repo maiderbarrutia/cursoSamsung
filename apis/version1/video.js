8615491b7d7f7470281ef7f36a72df5ad7c5a22c
MaiBarrutia <maiderbarrutia87@gmail.com>
2022-03-29 22:15:39 +0200

    añadir mensaje loading

--- apis/version1/video.js
+++ apis/version1/video.js
@@ -1,28 +1,28 @@
 "use strict";
 
+const videoUpload = document.getElementById("videoUpload");
 function selectVideo(event) {
   const file = event.target.files[0];
+  //Si el navegador soporta las apis hace...
   if (window.File && window.FileReader && window.FileList) {
-    console.log("soportado");
-    //Mira todos los archivos subidos y por cada archivo hace...
-    const message = document.querySelector("#messages");
-    const video = document.querySelector("#video");
+    const errorMessage = document.getElementById("errorMessage");
+    const loadingMessage = document.getElementById("loadingMessage");
+    const video = document.getElementById("video");
     // Aparece mensaje de error si el archivo no es de tipo video
     if (!file.type.match("video.*")) {
-      message.innerHTML = `<p class="error">Error! El tipo de archivo subido no es de video. Pruebe a subir de nuevo!</p>`;
+      errorMessage.innerHTML = `<p class="error">Error! El tipo de archivo subido no es de tipo video. Pruebe a subir de nuevo!</p>`;
       video.src = "";
     } else {
-      message.innerHTML = "";
+      errorMessage.innerHTML = "";
       const reader = new FileReader();
-      //Cargandose el archivo
+      //Cargandose el archivo...
       reader.addEventListener("progress", () => {
-        message.innerHTML = `<p class="loading">Cargando...</p>`;
+        loadingMessage.innerHTML = `<div class="loading"><span class="loader"></span><p>Cargando...</p></div>`;
       });
-      //Al subir el archivo...
+      //Al cargarse el archivo...
       reader.addEventListener("load", () => {
         const buttons = document.querySelector(".buttons");
-        const loadingMessage = document.querySelector(".loading");
-        loadingMessage.style.display = "none";
+        loadingMessage.innerHTML = "";
         video.src = reader.result;
         buttons.style.display = "block";
         videoButtons();
@@ -31,29 +31,30 @@ function selectVideo(event) {
       reader.readAsDataURL(file);
     }
   } else {
-    alert("Tu navegador no soporta el archivo subido!");
+    alert("Tu navegador no soporta las APIS!");
   }
 }
 
 function videoButtons() {
-  const video = document.querySelector("#video");
-  document.querySelector("#play").addEventListener("click", () => {
+  const video = document.getElementById("video");
+  document.getElementById("play").addEventListener("click", () => {
     video.play();
   });
 
-  document.querySelector("#pause").addEventListener("click", () => {
+  document.getElementById("pause").addEventListener("click", () => {
     video.pause();
   });
 
-  document.querySelector("#upVolume").addEventListener("click", () => {
+  document.getElementById("upVolume").addEventListener("click", () => {
     if (video.volume < 1) {
       video.volume += 0.1;
     }
   });
 
-  document.querySelector("#downVolume").addEventListener("click", () => {
+  document.getElementById("downVolume").addEventListener("click", () => {
     if (video.volume >= 0.1) {
       video.volume -= 0.1;
     }
   });
 }
+videoUpload.addEventListener("change", selectVideo);
